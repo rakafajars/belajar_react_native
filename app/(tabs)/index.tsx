b649637e905dcb1349a3e2e34613bec238c905d7
Raka Fajar <rakafajar18@gmail.com>
2024-05-11 08:59:59 +0700

    Add First Commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,119 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+// function component
+export default function App() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+
+      {/* <View style={styles.card}>
+        <View style={styles.content}>
+          <Text>Hello World</Text>
+        </View>
+      </View> */}
+
+
+      {/* Belajar FlexBox */}
+      {/* 1. Kotak Urut ke bawah */}
+      <View style={styles.wrapperAtas}>
+        {/* Kotak Merah */}
+        <View style={styles.merah}>
+          <Text>Merah</Text>
+        </View >
+        {/* Kotak Biru */}
+        <View style={styles.biru}>
+          <Text>Biru</Text>
+        </View>
+        {/* Kotak Hijau */}
+        <View style={styles.hijau}>
+          <Text>Hijau</Text>
+        </View>
+      </View>
+
+      {/* 2. Kotak Urut ke samping */}
+      <View style={styles.wrapperKotak}>
+        {/* Kotak Merah */}
+        <View style={styles.merah}>
+          <Text>Merah</Text>
+        </View >
+        {/* Kotak Biru */}
+        <View style={styles.biru}>
+          <Text>Biru</Text>
+        </View>
+        {/* Kotak Hijau */}
+        <View style={styles.hijau}>
+          <Text>Hijau</Text>
+        </View>
+      </View>
+
+      {/* 3. Kotak Urut samping ke tengah */}
+      <View style={styles.wrapperTengah}>
+        {/* Kotak Merah */}
+        <View style={styles.merah}>
+          <Text>Merah</Text>
+        </View >
+        {/* Kotak Biru */}
+        <View style={styles.biru}>
+          <Text>Biru</Text>
+        </View>
+        {/* Kotak Hijau */}
+        <View style={styles.hijau}>
+          <Text>Hijau</Text>
+        </View>
+      </View>
+
+
+
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 30,
+  },
+  merah: {
+    backgroundColor: 'red',
+    width: 100,
+    height: 100,
+  },
+  biru: {
+    backgroundColor: 'blue',
+    width: 100,
+    // height: 70
+    height: 100
+
+  },
+  hijau: {
+    backgroundColor: 'green',
+    width: 100,
+    height: 100
+  },
+  wrapperAtas: {
     alignItems: 'center',
-    gap: 8,
+    backgroundColor: 'yellow',
+    flex:1,
+    justifyContent:'center',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  wrapperKotak: {
+    flexDirection: 'row', 
+    justifyContent: 'flex-end',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  wrapperTengah: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    // alignItems: 'center',
+  }
+  // card: {
+  //   backgroundColor: 'red',
+  //   padding: 30,
+  //   margin: 20,
+  // },
+  // content: {
+  //   backgroundColor: 'yellow',
+  // }
 });
